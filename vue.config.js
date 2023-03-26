1bf0e0856d97716fa1cb4afaad12b2c3c9b44ae2
ArickCodeGuy <arickmail1997@gmail.com>
2023-03-26 13:23:00 +0300

    gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
-  publicPath: '/test-sknt/',
+  publicPath: '/test_sknt/',
   transpileDependencies: true,
 });
